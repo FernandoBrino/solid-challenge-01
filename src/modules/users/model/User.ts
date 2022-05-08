9a02345828d81804be3036a3411853cd70bbd699
Fernando Brino <fernandobrino11@gmail.com>
2022-05-07 22:24:24 -0300

    Intiate default values User model

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -1,12 +1,17 @@
 import { v4 as uuidV4 } from "uuid";
 
 class User {
-    id: string; 
-    name: string;
-    admin: boolean;
-    email: string;
-    created_at: Date;
-    updated_at: Date;
+  id: string;
+  name: string;
+  admin: boolean;
+  email: string;
+  created_at: Date;
+  updated_at: Date;
+
+  constructor() {
+    this.id = uuidV4();
+    this.admin = false;
+  }
 }
 
 export { User };
