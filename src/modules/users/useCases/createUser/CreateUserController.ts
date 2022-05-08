09d68b75347cd2316e36b241992d8dd204f0c016
Fernando Brino <fernandobrino11@gmail.com>
2022-05-08 11:27:36 -0300

    Create handle method in CreateUserController

--- src/modules/users/useCases/createUser/CreateUserController.ts
+++ src/modules/users/useCases/createUser/CreateUserController.ts
@@ -3,11 +3,15 @@ import { Response, Request } from "express";
 import { CreateUserUseCase } from "./CreateUserUseCase";
 
 class CreateUserController {
-  constructor(private createUserUseCase: CreateUserUseCase) {}
+    constructor(private createUserUseCase: CreateUserUseCase) {}
 
-  handle(request: Request, response: Response): Response {
-    // Complete aqui
-  }
+    handle(request: Request, response: Response): Response {
+        const { name, email } = request.body;
+
+        const user = this.createUserUseCase.execute({ name, email });
+
+        return response.status(201).json(user);
+    }
 }
 
 export { CreateUserController };
