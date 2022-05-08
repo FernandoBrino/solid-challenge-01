5e53b0ecc30c7953a96c1c50fe560aab597ed151
Fernando Brino <fernandobrino11@gmail.com>
2022-05-08 11:26:43 -0300

    CreateUserUserCase execute

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -2,16 +2,16 @@ import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  name: string;
-  email: string;
+    name: string;
+    email: string;
 }
 
 class CreateUserUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+    constructor(private usersRepository: IUsersRepository) {}
 
-  execute({ email, name }: IRequest): User {
-    // Complete aqui
-  }
+    execute({ email, name }: IRequest): User {
+        return this.usersRepository.create({ email, name });
+    }
 }
 
 export { CreateUserUseCase };
