56f8d4844100292dfe62160d79c414dfb38ea1f4
Fernando Brino <fernandobrino11@gmail.com>
2022-05-11 21:17:03 -0300

    List all users use case done

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -2,15 +2,24 @@ import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  user_id: string;
+    user_id: string;
 }
 
 class ListAllUsersUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+    constructor(private usersRepository: IUsersRepository) {}
 
-  execute({ user_id }: IRequest): User[] {
-    // Complete aqui
-  }
+    execute({ user_id }: IRequest): User[] {
+        const user = this.usersRepository.findById(user_id);
+        const userIsAdmin = user.admin === true;
+
+        if (!user || !userIsAdmin) {
+            throw new Error("Apenas admin's podem listar usuários");
+        }
+
+        const allUsers = this.usersRepository.list();
+
+        return allUsers;
+    }
 }
 
 export { ListAllUsersUseCase };
