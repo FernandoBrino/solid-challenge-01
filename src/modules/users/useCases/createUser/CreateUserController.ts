6c09535a939f5b56f4cf37e2f2127d38bfc3f145
Fernando Brino <fernandobrino11@gmail.com>
2022-05-11 21:17:52 -0300

    Create user controller done

--- src/modules/users/useCases/createUser/CreateUserController.ts
+++ src/modules/users/useCases/createUser/CreateUserController.ts
@@ -6,11 +6,13 @@ class CreateUserController {
     constructor(private createUserUseCase: CreateUserUseCase) {}
 
     handle(request: Request, response: Response): Response {
-        const { name, email } = request.body;
-
-        const user = this.createUserUseCase.execute({ name, email });
-
-        return response.status(201).json(user);
+        try {
+            const { email, name } = request.body;
+            const user = this.createUserUseCase.execute({ email, name });
+            return response.status(201).json(user);
+        } catch (err) {
+            return response.status(400).json({ error: err });
+        }
     }
 }
 
