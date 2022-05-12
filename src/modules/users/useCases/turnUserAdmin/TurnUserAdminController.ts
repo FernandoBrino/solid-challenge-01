68a2c7dd193f039d5103ba3adb2479e02f9909f7
Fernando Brino <fernandobrino11@gmail.com>
2022-05-11 21:18:18 -0300

    Turn use admin controller done

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -3,11 +3,17 @@ import { Request, Response } from "express";
 import { TurnUserAdminUseCase } from "./TurnUserAdminUseCase";
 
 class TurnUserAdminController {
-  constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
+    constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
-  handle(request: Request, response: Response): Response {
-    // Complete aqui
-  }
+    handle(request: Request, response: Response): Response {
+        try {
+            const { user_id } = request.body;
+            const user = this.turnUserAdminUseCase.execute(user_id);
+            return response.status(200).json(user);
+        } catch (err) {
+            return response.status(404).json({ error: err });
+        }
+    }
 }
 
 export { TurnUserAdminController };
