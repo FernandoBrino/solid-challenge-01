06a6e0583b6123b6a41ca412896b96af3aca718a
Fernando Brino <fernandobrino11@gmail.com>
2022-05-11 21:17:35 -0300

    Turn user admin use case done

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -2,15 +2,22 @@ import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  user_id: string;
+    user_id: string;
 }
 
 class TurnUserAdminUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+    constructor(private usersRepository: IUsersRepository) {}
 
-  execute({ user_id }: IRequest): User {
-    // Complete aqui
-  }
+    execute({ user_id }: IRequest): User {
+        const user = this.usersRepository.findById(user_id);
+
+        if (!user) {
+            throw new Error("Usuário não encontrado");
+        }
+
+        const userAdmin = this.usersRepository.turnAdmin(user);
+        return userAdmin;
+    }
 }
 
 export { TurnUserAdminUseCase };
