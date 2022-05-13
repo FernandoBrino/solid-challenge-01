c39607f9314be2f5ccefaedf443a75153cdf4cf3
Fernando Brino <fernandobrino11@gmail.com>
2022-05-12 22:28:53 -0300

    Show user profile controller done

--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -3,11 +3,17 @@ import { Request, Response } from "express";
 import { ShowUserProfileUseCase } from "./ShowUserProfileUseCase";
 
 class ShowUserProfileController {
-  constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
+    constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
 
-  handle(request: Request, response: Response): Response {
-    // Complete aqui
-  }
+    handle(request: Request, response: Response): Response {
+        try {
+            const { user_id } = request.params;
+            const user = this.showUserProfileUseCase.execute({ user_id });
+            return response.status(200).json(user);
+        } catch (err) {
+            return response.status(404).json({ error: err });
+        }
+    }
 }
 
 export { ShowUserProfileController };
