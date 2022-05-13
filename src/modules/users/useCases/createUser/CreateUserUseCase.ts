e2b7685565f7efc54d33b8e28705f6d07cfdb150
Fernando Brino <fernandobrino11@gmail.com>
2022-05-12 22:28:27 -0300

    Fix create user use case logic

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -10,12 +10,13 @@ class CreateUserUseCase {
     constructor(private usersRepository: IUsersRepository) {}
 
     execute({ email, name }: IRequest): User {
-        const user = this.usersRepository.create({ email, name });
         const emailExists = this.usersRepository.findByEmail(email);
 
         if (emailExists) {
             throw new Error("Um usuário com esse email já existe");
         }
+
+        const user = this.usersRepository.create({ email, name });
 
         return user;
     }
