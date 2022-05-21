0f30f90614c8054f78f8d1f80d4566ca0cf9f1b8
Fernando Brino <fernandobrino11@gmail.com>
2022-05-20 23:12:00 -0300

    Post route documentation swagger

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
+import swaggerFile from "../swagger.json";
 import { usersRoutes } from "./routes/users.routes";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use("/users", usersRoutes);
 
